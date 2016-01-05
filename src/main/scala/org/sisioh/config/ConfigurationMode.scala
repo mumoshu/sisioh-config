a6f02ec4d21e6e7d61db2e1bcdf180fefc563d7c
Yusuke Kuoka <kuoka@chatwork.com>
2016-01-05 15:41:26 +0900

    Add the extractor used in pattern matching to ease extracting ConfigurationMode values out of strings

--- src/main/scala/org/sisioh/config/ConfigurationMode.scala
+++ src/main/scala/org/sisioh/config/ConfigurationMode.scala
@@ -2,4 +2,11 @@ package org.sisioh.config
 
 object ConfigurationMode extends Enumeration {
   val Dev, Prod = Value
+
+  private val nameToMode = Map(
+    "development" -> Dev,
+    "production" -> Prod
+  )
+
+  def unapply(modeName: String): Option[Value] = nameToMode.get(modeName)
 }
